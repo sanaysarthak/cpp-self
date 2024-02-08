c250ffc7b16b39ffb6a5445bd97658b0b0f5d423
sanaysarthak <66907138+sanaysarthak@users.noreply.github.com>
2024-02-08 22:12:32 +0530

    Create q6.cpp

---
+++ NPTEL - Modern C++/Week 1/q6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	int iarr[] = {10, 20, 50, 40, 10, 50};
+	rotate(&iarr[0], &iarr[2], &iarr[6]);
+	// remove(&iarr[0], &iarr[6], 50);
+	remove(&iarr[0], &iarr[5], 50);
+	for(int i=0; i<4; ++i)
+		cout << iarr[i] << " ";
+	return 0;
+}
