85d37bba9f34cc8dca653686e47d8a9fa37ae937
sanaysarthak <66907138+sanaysarthak@users.noreply.github.com>
2024-02-08 22:12:48 +0530

    Create q7.cpp

---
+++ NPTEL - Modern C++/Week 1/q7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int iArr[] = {40, 50, 10, 30, 20};
+	sort(iArr, iArr + 4);
+	for(int i=0; i<5; i++)
+	{
+		cout << *(iArr + i) << " ";
+	}
+	return 0;
+}
