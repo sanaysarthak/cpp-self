2ce50f1bf98eae2669f119b19c18f2389325a08e
sanaysarthak <66907138+sanaysarthak@users.noreply.github.com>
2024-02-09 23:58:41 +0530

    Create q2.cpp

---
+++ NPTEL - Modern C++/Week 1/q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    char data[] = {'a', 'b', 'c', 'd', 'e'};
+    char key = 'd';
+    if(binary_search(data, data+5, key))
+        cout << "found";
+    else
+        cout << "not found";
+    return 0;
+}
