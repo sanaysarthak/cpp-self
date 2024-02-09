b828a59597147fbf3e237bd5a4d7069545c39573
sanaysarthak <66907138+sanaysarthak@users.noreply.github.com>
2024-02-10 00:00:05 +0530

    Update q2.cpp
    added another correct case scenario where the code works

--- NPTEL - Modern C++/Week 1/q2.cpp
+++ NPTEL - Modern C++/Week 1/q2.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     char data[] = {'a', 'b', 'c', 'd', 'e'};
     char key = 'd';
+    // if(binary_search(&data[0], &data[5], key)
     if(binary_search(data, data+5, key))
         cout << "found";
     else
