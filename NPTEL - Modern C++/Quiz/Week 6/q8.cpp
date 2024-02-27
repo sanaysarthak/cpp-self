d704b299ee01c2539a1b6bd090caa491268f342f
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-02-27 14:34:50 +0530

    added q8

--- NPTEL - Modern C++/Quiz/Week 6/q8.cpp
+++ NPTEL - Modern C++/Quiz/Week 6/q8.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class ClassA {
+class classA {
     public:
         virtual void f() { cout << "1 "; }
 };
-class ClassB : public 
+class classB : public classA {
+    public:
+        void f() { cout << "2 "; }
+};
+class classC : public classB {
+    public:
+        void f() { cout << "3 "; }
+};
+
+int main() {
+    classC *cb = new classC;
+    // Check which of the options will give 1 as output
+    // cb->f()
+    // classA::cb->f()
+    // cb->classA::f()
+    // classA::f()
+    return 0;
+}
